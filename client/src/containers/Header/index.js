36db98566b2d404d3484e4c420184339d27d8f84
deeppaz <deeppaz@hotmail.com>
2019-08-28 21:39:25 +0300

    update | added login, register on the nav

--- client/src/containers/Header/index.js
+++ client/src/containers/Header/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Container from '../../components/Container';
+import Menu from '../../components/Menu';
+
+const StyledNav = styled.nav`
+    background: white;
+    border-bottom: 1px solid ${props => props.theme.borderColor};
+`
+
+const StyledContainer = styled(Container)`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
 
 class Header extends React.Component {
     render(){
         return(
-            <div>this header component</div>
+            <StyledNav>
+                <StyledContainer>
+                    <Menu>
+                        <Menu.Item>
+                            Crypto Sozluk
+                        </Menu.Item>
+                    </Menu>
+                    <Menu>
+                        <Menu.Item>
+                            <Link to="/login">Giris Yap</Link>
+                        </Menu.Item>
+                    </Menu>
+                    <Menu>
+                        <Menu.Item>
+                            <Link to="/register">Uye ol</Link>
+                        </Menu.Item>
+                    </Menu>
+                </StyledContainer>
+            </StyledNav>
         )
     }
 }
--- client/src/tema.js
+++ client/src/tema.js
@@ -1,5 +1,18 @@
 const tema = {
-
+    borderColor: '#EDEFF1',
+    errorText: '#FF0000',
+    linkText: 'rgb(24, 144, 255);',
+    bodyText: '#1c1c1c',
+    bodyBackground: '#DAE0E6',
+    lightColor: '#FFFFFF',
+    metaText: '#7c7c7c',
+    
+    
+    fontSizeNormal: '14px',
+    fontSizeLarge: '20px',
+    fontSizeSmall: '12px',
+    
+    borderRadius: '4px',
 }
 
 export default tema;
